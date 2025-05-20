8f6e0981fb1b4f1f651ef87cefc2a716cf00e898
nserrano123 <nserrano@stone.com.co>
2025-05-20 11:11:24 -0500

    genera los embedings en supabase.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter'
 import { readFile } from 'fs/promises';
+import { createClient } from '@supabase/supabase-js'
+import { SupabaseVectorStore } from '@langchain/community/vectorstores/supabase';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import 'dotenv/config'; // Load environment variables
 
 try {
     // Read the local file
@@ -15,6 +19,23 @@ try {
     // Create documents from the text
     const output = await splitter.createDocuments([text]);
     console.log(output);
+    const sbApiKey = process.env.SUPABASE_API_KEY
+    const sbUrl = process.env.SUPABASE_URL_LC_CHATBOT
+    const openAIApiKey = process.env.OPENAI_API_KEY
+
+    //Crear cliente de supabase
+    const client = createClient(sbUrl, sbApiKey)
+    await SupabaseVectorStore.fromDocuments(output,
+        new OpenAIEmbeddings({ 
+            apiKey: openAIApiKey
+        }),
+        {
+            client,
+            tableName: 'documents'
+        }
+     )
+
+
   } catch (err) {
     console.error('Error:', err);
   }
